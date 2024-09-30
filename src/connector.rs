60c68052dbc4eb08c9cdd0adbb3f2b9f271265de
Conner Replogle <conner@replogle.dev>
2024-09-30 01:01:29 +0000

    c

--- src/connector.rs
+++ src/connector.rs
@@ -17,11 +17,11 @@ pub fn get_model_input_from_game<const W: usize, const L: usize>(
         .flatten()
         .collect();
     let tensor = Tensor::from_vec(state, (W, L, 4), &device)?;
-    debug!("Shape: {:?} Before {:?}", tensor.shape(),tensor.to_vec3::<f32>()?);
+    // debug!("Shape: {:?} Before {:?}", tensor.shape(),tensor.to_vec3::<f32>()?);
     let output_tensor = tensor.transpose(0, 2)?.transpose(1, 2)?;
 
     
-    debug!("Shape: {:?} After {:?}", output_tensor.shape(),output_tensor.to_vec3::<f32>()?);
+    // debug!("Shape: {:?} After {:?}", output_tensor.shape(),output_tensor.to_vec3::<f32>()?);
 
     return Ok(output_tensor);
 }
